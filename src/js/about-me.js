e75d8833f01647acc560be0aa77ba6cb31621ff6
Aleksander <hurieiev.al@gmail.com>
2024-07-06 21:37:04 +0300

    build accordein

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -3,4 +3,6 @@ import 'accordion-js/dist/accordion.min.css';
 
 export const accordionAboutMe = new Accordion('.accordion-container', {
   duration: 500,
+  // openOnInit: [0],
+  collapse: false
 });
