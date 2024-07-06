f7d2fe9ec221963245e7ebb9a048ec5c4f73602f
Aleksander <hurieiev.al@gmail.com>
2024-07-06 16:50:26 +0300

    main

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,4 +1,6 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-export const accordionAboutMe = new Accordion('.accordion-container');
+export const accordionAboutMe = new Accordion('.accordion-container', {
+  duration: 500,
+});
