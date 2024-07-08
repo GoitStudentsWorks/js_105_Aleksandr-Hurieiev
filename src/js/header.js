a975fe3a1ad5cfa73ce1de16bca2e403719974c0
Nataliia Hordiienko <Natalihordienko@gmail.com>
2024-07-08 22:46:34 +0200

    Add smooth scroll to header links

--- src/js/header.js
+++ src/js/header.js
@@ -2,7 +2,16 @@ const refs = {
     menu: document.querySelector('.header-nav-menu'),
     menuItems: document.querySelector('.header-menu'),
     btnOrder: document.querySelector('.header-btn'),
-    btnBurger: document.querySelector('.open-menu-btn')
+    targetOrder: document.querySelector('#work-together'),
+    btnBurger: document.querySelector('.open-menu-btn'),
+    linkAboutMe: document.querySelector('#header-about-me'),
+    targetAboutMe: document.querySelector('#about_me'),
+    linkBenefits: document.querySelector('#header-benefits'),
+    targetBenefits: document.querySelector('#benefits'),
+    linkProjects: document.querySelector('#header-projects'),
+    targetProjects: document.querySelector('#projects'),
+    linkFaq: document.querySelector('#header-faq'),
+    targetFaq: document.querySelector('#faq-section'),
 }
 
 export const menuAnim = refs.menu.addEventListener('click', (e) => {
@@ -12,4 +21,39 @@ export const menuAnim = refs.menu.addEventListener('click', (e) => {
     } else {
         refs.menuItems.classList.add('hidden');
     }
+})
+
+export const scrollToAboutMe = refs.linkAboutMe.addEventListener('click', (e) => {
+    e.preventDefault();
+    refs.targetAboutMe.scrollIntoView({
+        behavior: 'smooth'
+    });
+})
+
+export const scrollToBenefits = refs.linkBenefits.addEventListener('click', (e) => {
+    e.preventDefault();
+    refs.targetBenefits.scrollIntoView({
+        behavior: 'smooth'
+    });
+})
+
+export const scrollToProjects = refs.linkProjects.addEventListener('click', (e) => {
+    e.preventDefault();
+    refs.targetProjects.scrollIntoView({
+        behavior: 'smooth'
+    });
+})
+
+export const scrollToFaq = refs.linkFaq.addEventListener('click', (e) => {
+    e.preventDefault();
+    refs.targetFaq.scrollIntoView({
+        behavior: 'smooth'
+    });
+})
+
+export const scrollToOrder = refs.btnOrder.addEventListener('click', (e) => {
+    e.preventDefault();
+    refs.targetOrder.scrollIntoView({
+        behavior: 'smooth'
+    });
 })
--- src/main.js
+++ src/main.js
@@ -2,4 +2,11 @@ import './js/projects.js';
 import './js/faq.js';
 import { accordionAboutMe } from './js/about-me';
 import { accordionFaq } from './js/faq';
-import { menuAnim } from './js/header';
+import {
+    menuAnim,
+    scrollToAboutMe,
+    scrollToBenefits,
+    scrollToProjects,
+    scrollToFaq,
+    scrollToOrder,
+} from './js/header';
