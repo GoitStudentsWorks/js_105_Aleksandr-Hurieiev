526ada5047bfa3b356b742a2cc07f4f4b5507abf
shram <shram2615@gmail.com>
2024-07-08 23:59:04 +0300

    addAboutSwiper

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -15,14 +15,41 @@ import Swiper from 'swiper/bundle';
 import 'swiper/swiper-bundle.css';
 
 export const swiperAboutMe = new Swiper('.swiper_about', {
-  slidesPerView: 2,
-  loop: true,
-    navigation: {
+    speed: 400,
+    spaceBetween: 0,
+    loop: true,
+    setWrapperSize: true,
+    
+  breakpoints: {
+    1440: {
+      slidesPerView: 6,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    320: {
+      slidesPerView: 2,
+    },
+  },
+  navigation: {
       nextEl: '.swiper_about-next',
       prevEl: '.swiper_about-prev',
     },
     keyboard: {
       enabled: true,
       onlyInViewport: true,
+  },
+  on: {
+      init: function () {
+        document.querySelector('.swiper_about-slide').style.backgroundColor =
+          '#ed3b44';
+      },
+      slideChange: function () {
+        const previousSlide = this.slides[this.previousIndex];
+        const activeSlide = this.slides[this.activeIndex];
+        previousSlide.style.background = '';
+        activeSlide.style.backgroundColor = '#ed3b44';
+      },
     },
 });
+
