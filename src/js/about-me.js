dfb3ab9af0aa22d634edb048ed11bd040d69ca9d
Aleksander <hurieiev.al@gmail.com>
2024-07-08 21:37:22 +0300

    btn

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -15,38 +15,14 @@ import Swiper from 'swiper/bundle';
 import 'swiper/swiper-bundle.css';
 
 export const swiperAboutMe = new Swiper('.swiper_about', {
-  // wrapperClass: 'swiper_about-wrapper',
-  // slideClass: 'swiper_about-slide',
-  // // Optional parameter
-  // slidesPerView: 3,
-  // // If we need pagination
-  // direction: 'horizontal',
-  // // Navigation arrows
-  // navigation: {
-  //   nextEl: '.swiper_about-next',
-  //   prevEl: '.swiper_about-prev',
-  // },
-  // keyboard: {
-  //   enabled: true,
-  //   onlyInViewport: true,
-  //   pageUpDown: true,
-  // },
-    // Optional parameters
-    speed: 500,
-    spaceBetween: 5,
-    grabCursor: true,
-    allowTouchMove: true,
-    direction: 'horizontal',
-    watchOverflow: true,
-  
-    // Navigation arrows
+  slidesPerView: 2,
+  loop: true,
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-     },
-      keyboard: {
+      nextEl: '.swiper_about-next',
+      prevEl: '.swiper_about-prev',
+    },
+    keyboard: {
       enabled: true,
       onlyInViewport: true,
-      pageUpDown: true,
     },
 });
