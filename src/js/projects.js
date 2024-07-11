e8a4e270db40b7bad8f1951e5090b709ddec5c6c
shram <shram2615@gmail.com>
2024-07-11 09:12:22 +0300

    jsProjects

--- src/js/projects.js
+++ src/js/projects.js
@@ -2,24 +2,55 @@
 import Swiper from 'swiper/bundle';
 import 'swiper/swiper-bundle.css';
 
-const swiperProjects = new Swiper('.swiper-projects', {
-  
-  // Optional parameters
-  speed: 500,
-  spaceBetween: 5,
-  grabCursor: true,
-  allowTouchMove: true,
-  direction: 'horizontal',
-  watchOverflow: true,
+   document.addEventListener('DOMContentLoaded', () => {
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-btn-next',
-    prevEl: '.swiper-btn-prev',
-   },
-    keyboard: {
-    enabled: true,
-    onlyInViewport: true,
-    pageUpDown: true,
-  },
-});
+    const swiperProjects = new Swiper('.swiper-projects', {
+      speed: 500,
+      spaceBetween: 5,
+      grabCursor: true,
+      allowTouchMove: true,
+      direction: 'horizontal',
+      watchOverflow: true,
+      navigation: {
+        nextEl: '.swiper-btn-next',
+        prevEl: '.swiper-btn-prev',
+      },
+      keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+        pageUpDown: true,
+      },
+      on: {
+        slideChange: () => {
+          updateHoverEffect();
+        },
+        init: () => {
+          updateHoverEffect();
+        }
+      }
+    });
+
+    function updateHoverEffect() {
+  
+      const slides = document.querySelectorAll('.swiper-slide');
+      
+      slides.forEach(slide => {
+        const items = slide.querySelectorAll('.item-link');
+        if (slide.classList.contains('swiper-slide-active')) {
+          let index = 0;
+          const intervalTime = 1000; 
+
+          function setHover() {
+            items.forEach(item => item.classList.remove('hover'));
+            items[index].classList.add('hover');
+            index = (index + 1) % items.length;
+          }
+          setInterval(setHover, intervalTime);
+        } else {
+          const items = slide.querySelectorAll('.item-link');
+          items.forEach(item => item.classList.remove('hover'));
+        }
+      });
+    }
+        updateHoverEffect();
+  });
